3d0ba4e6ea6bff20b551bcffa6abb0cbfe4c4282
0yaizu <kaira.oyaizu@gmail.com>
2026-07-07 02:37:29 +0900

    ✨ tamaguiを追加した

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,20 +1,14 @@
+import tamaguiConfig from '@tamagui/config';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider, Text, YStack } from 'tamagui';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
+      <YStack flex={1} backgroundColor="$background" justifyContent="center" alignItems="center">
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </YStack>
+    </TamaguiProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
